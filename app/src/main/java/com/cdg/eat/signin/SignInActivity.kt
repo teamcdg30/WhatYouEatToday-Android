27134d826c861dae59974f53fdc3f0d2812f0bef
ukhyunKwak <ukhyun91@gmail.com>
2019-08-11 23:57:26 +0900

    Update: Google Signin 진행중

--- app/src/main/java/com/cdg/eat/signin/SignInActivity.kt
+++ app/src/main/java/com/cdg/eat/signin/SignInActivity.kt
@@ -3,7 +3,10 @@ package com.cdg.eat.signin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cdg.eat.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 
 class SignInActivity : AppCompatActivity() {
@@ -14,10 +17,18 @@ class SignInActivity : AppCompatActivity() {
 
     private lateinit var googleSignInClient: GoogleSignInClient
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = FirebaseAuth.getInstance()
+        
     }
 }
 
